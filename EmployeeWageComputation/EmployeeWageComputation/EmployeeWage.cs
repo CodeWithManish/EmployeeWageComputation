bd1e3d26a48080360f45c91fa6014bda9eb165bd
Manish <manishkumar0156@gmail.com>
2022-02-02 19:11:45 +0530

    [Manish] added added total working days

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
@@ -9,32 +9,34 @@ namespace EmployeeWageComputation
     public class EmployeeWage
     {
         const int EMP_PRESENT = 1, EMP_ABSENT = 0, PART_TIME = 0, FULL_TIME = 1, WAGE_PER_HOUR = 20,
-            FULLDAYS_HOUR = 8, PART_TIME_HOUR = 4, WORKING_DAYS = 20;
-        int wagePerMonth = 0;
+              FULLDAYS_HOUR = 8, PART_TIME_HOUR = 4, WORKING_DAYS = 100;
+        int maxWagePerMonth = 0;
+
         public void EmpAttendence()
         {
             Random random = new Random();
-            int empcheck = random.Next(0, 3);
+            int empcheck = random.Next(0, 4);
+
             for (int i = 0; i < WORKING_DAYS; i++)
             {
 
                 switch (empcheck)
                 {
                     case FULL_TIME:
-                        wagePerMonth += WAGE_PER_HOUR * FULLDAYS_HOUR;
+                        maxWagePerMonth += WAGE_PER_HOUR * FULLDAYS_HOUR;
                         break;
 
                     case PART_TIME:
-                        wagePerMonth += WAGE_PER_HOUR * PART_TIME_HOUR;
+                        maxWagePerMonth += WAGE_PER_HOUR * PART_TIME_HOUR;
                         break;
 
                     default:
-
+                        maxWagePerMonth = 0;
                         break;
 
                 }
             }
-            Console.WriteLine("Monthy wage of Employee is " + wagePerMonth);
+            Console.WriteLine("Total Employee Wage :" + maxWagePerMonth);
         }
     }
 }
