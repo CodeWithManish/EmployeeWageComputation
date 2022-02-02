4c0d44449a786b6cc62f63b105cb30b0bf529dd3
Manish <manishkumar0156@gmail.com>
2022-02-02 19:03:00 +0530

    [Manish] added Monthly wage

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
@@ -9,32 +9,32 @@ namespace EmployeeWageComputation
     public class EmployeeWage
     {
         const int EMP_PRESENT = 1, EMP_ABSENT = 0, PART_TIME = 0, FULL_TIME = 1, WAGE_PER_HOUR = 20,
-             FULLDAYS_HOUR = 8, PART_TIME_HOUR = 4;
-
-        int wagePerDay = 0;
-
+            FULLDAYS_HOUR = 8, PART_TIME_HOUR = 4, WORKING_DAYS = 20;
+        int wagePerMonth = 0;
         public void EmpAttendence()
         {
             Random random = new Random();
             int empcheck = random.Next(0, 3);
-            switch (empcheck)
+            for (int i = 0; i < WORKING_DAYS; i++)
             {
-                case FULL_TIME:
-                    Console.WriteLine("Employee is present");
-                    wagePerDay = WAGE_PER_HOUR * FULLDAYS_HOUR;
-                    break;
 
-                case PART_TIME:
-                    Console.WriteLine("Part Time Employee is");
-                    wagePerDay = WAGE_PER_HOUR * PART_TIME_HOUR;
-                    break;
+                switch (empcheck)
+                {
+                    case FULL_TIME:
+                        wagePerMonth += WAGE_PER_HOUR * FULLDAYS_HOUR;
+                        break;
 
-                default:
-                    wagePerDay = 0;
-                    break;
+                    case PART_TIME:
+                        wagePerMonth += WAGE_PER_HOUR * PART_TIME_HOUR;
+                        break;
 
+                    default:
+
+                        break;
+
+                }
             }
-            Console.WriteLine("Employee wage: " + wagePerDay);
+            Console.WriteLine("Monthy wage of Employee is " + wagePerMonth);
         }
     }
 }
