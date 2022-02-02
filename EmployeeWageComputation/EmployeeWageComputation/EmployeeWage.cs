ac8725ab6794d4f6dea46171c9ce017206446038
Manish <manishkumar0156@gmail.com>
2022-02-02 18:44:58 +0530

    [Manish] Added Employee part time

--- EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
+++ EmployeeWageComputation/EmployeeWageComputation/EmployeeWage.cs
@@ -8,24 +8,34 @@ namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
-        const int EMP_PRESENT = 1, EMP_ABSENT = 0, WAGE_PER_HOUR = 20, FULLDAYS_HOUR = 8;
-        int dailyEmployeeWage = 0;
-        public void CalcEmployeeWage()
+        const int EMP_PRESENT = 1, EMP_ABSENT = 0, PART_TIME = 0, FULL_TIME = 1, WAGE_PER_HOUR = 20,
+             FULLDAYS_HOUR = 8, PART_TIME_HOUR = 4;
+
+        int wagePerDay = 0;
+
+        public void EmpAttendence()
         {
             Random random = new Random();
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
 
-            if (empcheck == EMP_PRESENT)
+            if (empcheck == PART_TIME_HOUR)
             {
-                Console.WriteLine("Employee is Present");
-                dailyEmployeeWage = WAGE_PER_HOUR * FULLDAYS_HOUR;
-                Console.WriteLine(dailyEmployeeWage);
+                Console.WriteLine("Part Time Employee is");
+                wagePerDay = WAGE_PER_HOUR * PART_TIME_HOUR;
+
             }
+            if (empcheck == FULLDAYS_HOUR)
+            {
+                Console.WriteLine("Employee is present");
+                wagePerDay = WAGE_PER_HOUR * FULLDAYS_HOUR;
+
+            }
+
             if (empcheck == EMP_ABSENT)
             {
-                Console.WriteLine("Employee is Absent");
-
+                wagePerDay = 0;
             }
+            Console.WriteLine("Employee wage: " + wagePerDay);
         }
     }
 }
